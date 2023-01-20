39d0ebf5c6117fd194c7beca6f1dc2c95ad33ebd
Michael Hu <43020369+GuessHuIAm@users.noreply.github.com>
2023-01-20 01:31:19 -0500

    created boilerplate code

---
+++ react-portfolio/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Header from './components/header/Header'
+import NavBar from './components/navbar/NavBar'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+import './index.css'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
